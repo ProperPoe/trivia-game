cb8066b111663596b1003ad45a5d4ab0947d9364
ProperPoe <Potencyalmighty@outlook.com>
2024-12-10 20:28:31 -0700

    Refactored Home page to route to quiz when button is clicked on

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import Header from '../components/Header';
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div style={{ textAlign: 'center' }}>
       <Header title="Welcome to the Trivia Game!" />
       <Button
         text="Start Quiz"
-        onClick={() => {
-          console.log('Start Quiz clicked!');
-        }}
+        onClick={() => navigate('/quiz')}
       />
     </div>
   );
