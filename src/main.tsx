b6e89a6799ed73822edd77d8bd2cad6b52f2ed90
ProperPoe <Potencyalmighty@outlook.com>
2024-12-16 22:53:51 -0700

    Changes for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_QZwMFsADI", // Replace with your Cognito User Pool URL
   client_id: "4s5lfoun5dvt9el0p52fuj0bau", // Replace with your App Client ID
-  redirect_uri: "https://main.d157strcljw05z.amplifyapp.com", // Your app's redirect URI
+  redirect_uri: "https://main.d157strcljw05z.amplifyapp.com", 
   response_type: "code",
   scope: "email openid phone",
 
